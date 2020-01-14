8ae5ff87c4fe6d22c86123b932fc9e1e8bea66be
Kajetan Krzewina <kajetan.krzewina@gmail.com>
2020-01-14 10:51:38 +0100

    decoder output trouble, first try at visualization

---
+++ README
@@ -0,0 +1,5 @@
+# HOW TO RUN TENSORBOARD
+tensorboard --logdir=runs
+
+# HOW TO MONITOR NVIDIA GPU
+nvidia-smi -lms 500
--- base.py
+++ base.py
@@ -7,22 +7,47 @@ import torch.nn.functional as F
 import torch.utils.data as Data
 import mdshare
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter('runs/tmp_1')
+
+def matplotlib_imshow(img):
+    """ using plt to plot img in tensorboard """
+    img = img.mean(dim=0)
+    npimg = img.cpu().detach().numpy()
+    plt.imshow(npimg, cmap="Greys")
 
 os.system("clear")
 # pylint: disable=E1101
 
 # Data load
-DATA = mdshare.fetch("alanine-dipeptide-3x250ns-heavy-atom-distances.npz")#, working_directory=None)
+DATA = mdshare.fetch("alanine-dipeptide-3x250ns-heavy-atom-distances.npz")
 with np.load(DATA) as f:
     dataset = np.vstack([f[key] for key in sorted(f.keys())])
 
+x_train, x_test = train_test_split(dataset, train_size=0.7)
+
 BATCH_SIZE = 1000
 BATCH_PART = 20
-EPOCHS = 25
+EPOCHS = 15
 
 # Data loader for easy mini-batch return in training
-train_loader = Data.DataLoader(dataset=dataset[::BATCH_PART], batch_size=BATCH_SIZE, shuffle=True)
-test_loader = Data.DataLoader(dataset=dataset[::BATCH_PART], batch_size=BATCH_SIZE, shuffle=True)
+train_loader = Data.DataLoader(
+    dataset=x_train,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+    )
+test_loader = Data.DataLoader(
+    dataset=x_test,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+    )
+
 
 class AutoEncoder(nn.Module):
     """ autoencoder class
@@ -31,25 +56,25 @@ class AutoEncoder(nn.Module):
             y variable
     """
 
-    #def __init__(self, x, y=None):
+    # def __init__(self, x, y=None):
     def __init__(self):
         """
         initializing autoencoder class
         """
         super(AutoEncoder, self).__init__()
 
-        #self.X = torch.from_numpy(x).type(torch.FloatTensor)
-        #self.y = y
+        # self.X = torch.from_numpy(x).type(torch.FloatTensor)
+        # self.y = y
 
-        self.layer1 = nn.Linear(45, 20)
-        self.layer2 = nn.Linear(20, 10)
-        self.layer2a = nn.Linear(10, 5)
-        self.layer2b = nn.Linear(5, 2)
+        self.input_layer = nn.Linear(45, 20)
+        self.encode_1 = nn.Linear(20, 10)
+        self.encode_2 = nn.Linear(10, 5)
+        self.encode_3 = nn.Linear(5, 2)
 
-        self.layer3b = nn.Linear(2, 5)
-        self.layer3a = nn.Linear(5, 10)
-        self.layer3 = nn.Linear(10, 20)
-        self.layer4 = nn.Linear(20, 45)
+        self.decode_1 = nn.Linear(2, 5)
+        self.decode_2 = nn.Linear(5, 10)
+        self.decode_3 = nn.Linear(10, 20)
+        self.output_layer = nn.Linear(20, 45)
 
         self.input = None
 
@@ -57,8 +82,6 @@ class AutoEncoder(nn.Module):
         """
         initialize forward method
         """
-        self.input = x
-
         x = self.encode(x)
         x = self.decode(x)
 
@@ -68,74 +91,27 @@ class AutoEncoder(nn.Module):
         """ prints variables """
         print("X:", self.X, "\ny:", self.y)
 
-    def _create_network(self):
-        """
-        initialize autoencode network weights and biases
-        """
-        # TO DO: implement here network creation
-
-    def _initialize_weights(self):
-        """
-        initialize weights
-        """
-        # TO DO: implement here weights initialization
-
     def encode(self, x):
         """
         encoding function
         """
 
-        h1 = F.relu(self.layer1(x))
-        h2 = F.relu(self.layer2(h1))
-        h3 = F.relu(self.layer2a(h2))
+        x = F.relu(self.input_layer(x))
+        x = F.relu(self.encode_1(x))
+        x = F.relu(self.encode_2(x))
 
-        return self.layer2b(h3)
+        return F.relu(self.encode_3(x))
 
     def decode(self, z):
         """
         decoding function
         """
 
-        h3 = F.relu(self.layer3b(z))
-        h2 = F.relu(self.layer3a(h3))
-        h1 = F.relu(self.layer3(h2))
+        z = F.relu(self.decode_1(z))
+        z = F.relu(self.decode_2(z))
+        z = F.relu(self.decode_3(z))
 
-        return torch.relu(self.layer4(h1))
-
-    def _loss_optimize(self):
-        """
-        loss optimizing function
-        """
-        # TO DO: implement here loss optimizing function
-
-    def _transform(self):
-        """
-        transformation data into latent space
-        """
-        # TO DO: implement here transformation function
-
-    def _fit(self):
-        """
-        feed function
-        """
-        # TO DO: implement here fit function
-
-    def _predict(self):
-        """
-        predict function
-        """
-        # TO DO: implement here predict function
-
-    def lossfunction(self):
-        """
-        implement here lossfunction (output)
-        """
-        return nn.MSELoss(reduction='sum')
-
-    #def train(self):
-    #    """
-    #    implement here train function
-    #    """
+        return F.relu(self.output_layer(z))
 
     def trainIters(self, mod, optim):
         """
@@ -143,16 +119,18 @@ class AutoEncoder(nn.Module):
         """
         def train_step(x):
             mod.train()
-            predict = model(x)
+            predict = mod(x)
+
+            optim.zero_grad()
 
             los = self.lossfunction()
-            los = loss(self.input, predict)
+            los = los(self.input, predict)
+
             los.backward()
 
             optim.step()
-            optim.zero_grad()
 
-            return loss.item()
+            return los.item()
 
         return train_step
 
@@ -160,17 +138,47 @@ class AutoEncoder(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = AutoEncoder().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+criterion = torch.nn.MSELoss()
 
-train = model.trainIters(model, optimizer)
+# train = model.trainIters(model, optimizer)
 losses = []
 
-for epoch in range(1, EPOCHS + 1):
-    for batch_idx, data in enumerate(train_loader):
-        data = data.to(device)
-        batch = model(data)
-        loss = train(batch)
-        losses.append(loss)
-        print('loss {:.24f}'.format(loss))
-    print(epoch)
+# Set model into train mode
+model.train()
 
-print(model.state_dict())
+# train
+for epoch in range(EPOCHS):
+    batch_loss = 0.0
+    for batch_idx, data in enumerate(train_loader):
+        # print(f'Epoch: {epoch} batch_idx: {batch_idx}')
+        data = data.to(device)
+        output = model(data)
+        loss = criterion(output, data)
+        batch_loss += loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print('epoch: {}\t'.format(epoch + 1) \
+        + 'avg loss: {:.5f}\t'.format(batch_loss / 525) \
+        + 'loss: {:.5f}\t'.format(batch_loss))
+
+
+
+# Set model into evaluation mode
+model.eval()
+for batch_idx, data in enumerate(test_loader):
+    data = data.to(device)
+    output = model(data)
+    #print(f'Data {data} \nOutput {output}')
+
+    #dataiter = iter(data)
+    #input_img = dataiter.next()
+    img_input_grid = torchvision.utils.make_grid(data)
+    matplotlib_imshow(img_input_grid)
+    writer.add_image('input_data', img_input_grid, batch_idx)
+    print(data[0:10])
+
+    img_output_grid = torchvision.utils.make_grid(output)
+    matplotlib_imshow(img_output_grid)
+    writer.add_image('output_data', img_output_grid, batch_idx)
+    print(output[0:10])
